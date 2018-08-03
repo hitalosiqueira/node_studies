b8ec94c83c1f87feb0af03c663f2d5a91c0a9d95
Hitalo Siqueira <hsiqueira10@gmail.com>
2018-08-03 19:05:40 -0300

    styles

--- node-chat-app/server/utils/message.test.js
+++ node-chat-app/server/utils/message.test.js
@@ -1,4 +1,4 @@
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generate message', () => {
     test('should generate correct message object', () => {
@@ -7,6 +7,17 @@ describe('generate message', () => {
         expect(message).toBeDefined();
         expect(message.from).toBe('hitalo');
         expect(message.text).toBe('hello world');
+        expect(message.createdAt).toBeDefined();
+    });
+});
+
+describe('generate location message', () => {
+    test('should generate correct location message object', () => {
+        var message = generateLocationMessage('hitalo', 0, 0);
+        
+        expect(message).toBeDefined();
+        expect(message.from).toBe('hitalo');
+        expect(message.url).toBe('https://www.google.com/maps?q=0,0');
         expect(message.createdAt).toBeDefined();
     });
 });
