26a167f2f11bdbb11383c43a37dd601690e0168d
Hitalo Siqueira <hsiqueira10@gmail.com>
2018-08-03 23:11:35 -0300

    scrolling messages

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -10,21 +26,30 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formatedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  li.text(`${message.from} ${formatedTime}: ${message.text}`);
-  $('#messages').append(li);
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formatedTime
+  });
+
+  $('#messages').append(html);
+  scrollToBottom();
 
 });
 
 socket.on('newLocationMessage', function (message) {
+
   var formatedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  var a = $('<a target="_blank">My current location</a>')
-  
-  li.text(`${message.from} ${formatedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  $('#messages').append(li);
+  var template = $('#location-message-template').html();
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formatedTime
+  });
+
+  $('#messages').append(html);
+  scrollToBottom();
 });
 
 $("#message-form").submit(function (e) {
